62530d79e6b49ebc7f229620c4752c344d4f2683
aorews <aorew@yandex.ru>
2023-03-04 17:06:05 +0300

    config_reader

---
+++ instloader/config_reader.py
@@ -0,0 +1,59 @@
+import json
+from dataclasses import dataclass
+from datetime import datetime
+
+
+class ConfigParser:
+    def __init__(self, path):
+        self.path = path
+        self.config = self.read_config()
+
+    def read_config(self):
+        with open(self.path, "r") as f:
+            data = json.load(f)
+        return data
+
+    def write_config(self):
+        with open(self.path, "w") as f:
+            json.dump(self.config, f)
+
+
+@dataclass
+class InstagramPage:
+    link: str
+    post_photo: bool
+    post_video: bool
+    stories_photo: bool
+    stories_video: bool
+    date_from: datetime
+
+
+class InstagramConfig(ConfigParser):
+    def __init__(self, path):
+        super().__init__(path)
+        self.download_config = self.get_download_config()
+
+    def get_download_config(self) -> list:
+        config = list()
+        self.get_dates()
+
+        for link_config in self.config["Links"]:
+            config.append(self.get_instagram_page(link_config))
+        return config
+
+    def get_dates(
+        self,
+    ):
+        date = self.config["Date_from"]
+        self.date_from = datetime.fromisoformat(date)
+
+    def get_instagram_page(self, link_config: dict) -> InstagramPage:
+        page = InstagramPage(
+            link=link_config["Instagram Link"],
+            post_photo="VK album link post photo" in link_config,
+            post_video="VK album link post video" in link_config,
+            stories_photo="VK album link stories photo" in link_config,
+            stories_video="VK album link stories video" in link_config,
+            date_from=self.date_from,
+        )
+        return page
---
+++ tests/config_tests/test_configs.py
@@ -0,0 +1,65 @@
+import pytest
+from pathlib import Path
+from instloader.config_reader import ConfigParser, InstagramConfig, InstagramPage
+from datetime import datetime
+
+PATH_TO_CONFIGS = "tests/config_tests/configs/"
+
+
+@pytest.mark.parametrize(
+    "config_name, expected",
+    [
+        (
+            "creds.json",
+            {
+                "vk": {"login": "test_vk", "pass": "test_vk_pass"},
+                "inst": {"login": "test_inst", "pass": "test_inst_pass"},
+            },
+        ),
+        (
+            "links_test.json",
+            {
+                "Date_from": "2023-02-01T18:10:53",
+                "Links": [
+                    {
+                        "Instagram Link": "https://www.instagram.com/test1/",
+                        "VK album link post photo": "vk.com/group12",
+                    },
+                    {
+                        "Instagram Link": "https://www.instagram.com/test2/",
+                        "VK album link stories video": "vk.com/group1",
+                    },
+                ],
+            },
+        ),
+    ],
+)
+def test_config_reading(config_name, expected):
+    path = Path(PATH_TO_CONFIGS, config_name)
+    config = ConfigParser(path=path).config
+
+    assert config == expected
+
+
+def test_instagram_config():
+    path = Path(PATH_TO_CONFIGS, "links_test.json")
+    download_config = InstagramConfig(path).download_config
+
+    assert download_config == [
+        InstagramPage(
+            link="https://www.instagram.com/test1/",
+            post_photo=True,
+            post_video=False,
+            stories_photo=False,
+            stories_video=False,
+            date_from=datetime(2023, 2, 1, 18, 10, 53),
+        ),
+        InstagramPage(
+            link="https://www.instagram.com/test2/",
+            post_photo=False,
+            post_video=False,
+            stories_photo=False,
+            stories_video=True,
+            date_from=datetime(2023, 2, 1, 18, 10, 53),
+        ),
+    ]
